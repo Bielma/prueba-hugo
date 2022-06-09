0c366529fcb36d00a5287fdc53a44ba10b09e3ab
bielma <abielmalopez@gmail.com>
2022-06-09 11:28:50 -0500

    List active users

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import api from './services/api';
 
 function App() {
+  const [users, setUsers] = useState([])
+  const headers = [
+    "ID",
+    "Name",
+    "Email",
+    "Gender",
+    "Status"
+  ]
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData= async()=>{
+    const response = await api.users.getUsers()
+    if(response.success){
+      const activeUsers = response.filter(user=>user.status === 'active')
+      setUsers(activeUsers)
+      console.log(activeUsers)
+      console.log(Object.keys(activeUsers[0]))
+
+    }
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <p>
+         Usuarios activos
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <table>
+          <tr>
+            {
+              headers.map(i=>(
+                <th>{i}</th>  
+              ))
+            }
+          </tr>
+          {
+            users?.map(user=>(
+              <tr>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.gender}</td>
+              <td>{user.status}</td>
+              </tr> 
+            ))
+          }
+          
+        </table>
+
       </header>
     </div>
   );
